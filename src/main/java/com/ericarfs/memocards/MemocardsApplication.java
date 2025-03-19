1e3ebf2d8eefcc2302a7b1db16e446cdf3228016
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-19 20:19:29 -0300

    start project

---
+++ src/main/java/com/ericarfs/memocards/MemocardsApplication.java
@@ -0,0 +1,13 @@
+package com.ericarfs.memocards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemocardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemocardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericarfs/memocards/MemocardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericarfs.memocards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemocardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
