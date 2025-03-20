1d50877ec46d6c2807163a79a9b7a9e551532ea8
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-20 14:57:47 -0300

    Adding Error Handling

---
+++ src/main/java/com/ericarfs/memocards/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.ericarfs.memocards.exceptions;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return new ResponseEntity<>(err, status);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleGenericException(Exception ex){
+		Map <String, Object> body = new LinkedHashMap<>();
+		
+		body.put("timestamp", LocalDateTime.now());
+		body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		body.put("error", "Internal Server Error");
+		body.put("message", ex.getMessage());
+		
+		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
