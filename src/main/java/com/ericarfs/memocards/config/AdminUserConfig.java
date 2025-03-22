f9f46157e583a4cdaf6847f9f476ec8d37c9ca14
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-22 01:43:13 -0300

    Adding the Admin User Configuration class

---
+++ src/main/java/com/ericarfs/memocards/config/AdminUserConfig.java
@@ -0,0 +1,44 @@
+package com.ericarfs.memocards.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ericarfs.memocards.entity.User;
+import com.ericarfs.memocards.entity.enums.Role;
+import com.ericarfs.memocards.repository.UserRepository;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public AdminUserConfig(UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                user -> {
+                    System.out.println("admin ja existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(passwordEncoder.encode("123"));
+                    user.setRole(Role.ADMIN);
+                    userRepository.save(user);
+                }
+        );
+    }
+}
