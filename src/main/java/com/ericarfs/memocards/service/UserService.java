38bfba15e1776e700af4a6afeeea28cf6205bb0d
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-22 01:43:32 -0300

    Updating UserService to add auth methods

--- src/main/java/com/ericarfs/memocards/service/UserService.java
+++ src/main/java/com/ericarfs/memocards/service/UserService.java
@@ -1,11 +1,14 @@
 package com.ericarfs.memocards.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ericarfs.memocards.entity.User;
+import com.ericarfs.memocards.entity.enums.Role;
 import com.ericarfs.memocards.exceptions.ResourceNotFoundException;
 import com.ericarfs.memocards.repository.UserRepository;
 
@@ -14,7 +17,10 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public List<User> findAll() {
 		return repository.findAll();
 	}
@@ -22,10 +28,6 @@ public class UserService {
 	public User findById(String id) {
 		return repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found."));
-	}
-
-	public void insert(User obj) {
-		repository.insert(obj);
 	}
 	
 	public void delete(String id) {
@@ -36,4 +38,17 @@ public class UserService {
 
 	}
 
+	public void register(String username, String password) {
+		String encryptedPassword = passwordEncoder.encode(password);
+		User user = new User(null, username, encryptedPassword, Role.BASIC);
+		repository.insert(user);
+	}
+	
+	public Boolean validatePassword(String userPassword, String password) {
+		return passwordEncoder.matches(password, userPassword);
+	}
+	
+	public Optional<User> findByUsername(String username) {
+		return repository.findByUsername(username);
+	}
 }
